DemianWright/io_scene_blb

commit 21c7ad4ebd0797d76e27fc290e1c1d4622b2c692

Using enum with brick texture names, uppercasing input instead of lower

blb_processor.py
@@ -567,7 +567,7 @@ def __sequence_z_to_plates(xyz, plate_height):
         return xyz
 
 
-def __split_object_name_to_tokens(name, replace_commas=False):
+def __split_object_string_to_tokens(name, replace_commas=False):
     """Splits a Blender object name into its token parts.
     Correctly takes into account duplicate object names with .### at the end.
 
@@ -576,7 +576,7 @@ def __split_object_name_to_tokens(name, replace_commas=False):
         replace_commas (bool): Replace all commas with periods in the object name? (Default: False)
 
     Returns:
-        The name split into a list of strings at whitespace characters.
+        The name split into a list of uppercase strings at whitespace characters.
     """
     if replace_commas:
         name = name.replace(",", ".")
@@ -586,10 +586,10 @@ def __split_object_name_to_tokens(name, replace_commas=False):
     # When the name is split at whitespace, the first object is recognized as a grid definition object and the second is not.
     if len(name) > 4 and name[-4] == ".":
         # Remove the last 4 characters of from the name before splitting at whitespace.
-        tokens = name[:-4].lower().split()
+        tokens = name[:-4].upper().split()
     else:
         # Split the object name at whitespace.
-        tokens = name.lower().split()
+        tokens = name.upper().split()
 
     return tokens
 
@@ -605,9 +605,12 @@ def __get_tokens_from_object_name(name, tokens):
         A set of tokens that exist both in the Blender object name and the specified sequence of tokens, in the order they appeared in the name.
     """
     if isinstance(name, str):
-        name_tokens = __split_object_name_to_tokens(name)
+        name_tokens = __split_object_string_to_tokens(name)
     else:
         name_tokens = name
+
+    # In case tokens sequence contains mixed case characters, convert everything to uppercase.
+    tokens = [token.upper() for token in tokens]
 
     # Convert name tokens and wanted tokens into sets.
     # Get their intersection.
@@ -1598,12 +1601,12 @@ def __calc_quad_max_edge_len_idx(sorted_verts):
     return (max_len, max_len_idx)
 
 
-def __calculate_uvs(texture_name, vert_coords, normal, forward_axis):
+def __calculate_uvs(brick_texture, vert_coords, normal, forward_axis):
     """Calculates the UV coordinates for the specified texture and quad containing the specified vertices.
     In unsupported cases, default UVs are returned.
 
     Args:
-        texture_name (string): A value from const.VALID_BRICK_TEXTURES.
+        brick_texture (BrickTexture): A value from the BrickTexture enum.
         vert_coords (sequence of coordinates): A sequence of 4 local space coordinates of a face in CW order.
                                               The vertex order must be the same that is written to the BLB file.
         normal (sequence of numbers): The normal vector of the quad.
@@ -1635,7 +1638,7 @@ def __calculate_uvs(texture_name, vert_coords, normal, forward_axis):
 
     # Sanity check.
     if len(vert_coords) < 4:
-        raise RuntimeError("__calculate_uvs(texture_name, vert_coords, normal) function expects a quad, input polygon was not a quad.")
+        raise RuntimeError("__calculate_uvs(brick_texture, vert_coords, normal) function expects a quad, input polygon was not a quad.")
 
     idx_coord = [(idx, coord) for idx, coord in enumerate(vert_coords)]
 
@@ -1707,7 +1710,7 @@ def __calculate_uvs(texture_name, vert_coords, normal, forward_axis):
     # UV tuples are in order: (u, v)
     # Where u is the x axis increasing from left to right.
     # Where v is the y axis increasing from top to bottom.
-    if texture_name == "top":
+    if brick_texture is const.BrickTexture.TOP:
         # Works well for axis aligned faces horizontal faces.
         # Works well enough for axis aligned faces vertical faces.
         # Doesn't look bad for all other face orientations.
@@ -1737,7 +1740,7 @@ def __calculate_uvs(texture_name, vert_coords, normal, forward_axis):
         else:
             uvs_blender = uvs_sorted
 
-    elif texture_name == "side":
+    elif brick_texture is const.BrickTexture.SIDE:
         # To calculate the UV coordinates for a non-rectangular quad, the and U and V components must be calculated separately for each side.
         # Calculate the UV components for top, left, right, and bottom edges of the quad.
         # If the quad is rectangular then the components of opposing sides are equal.
@@ -1757,7 +1760,9 @@ def __calculate_uvs(texture_name, vert_coords, normal, forward_axis):
                       (1 - u_t, v_r),
                       (1 - u_b, 1 - v_r),
                       (u_b, 1 - v_l))
-    elif texture_name == "bottomedge":
+    elif brick_texture is const.BrickTexture.BOTTOMEDGE:
+        # FIXME: Blender uvs broken. Widths are swapped + has Y offset. BLB uvs OK.
+
         # Bottom edge is a special case where the average width/height does not work and the top left may not be what was determined by the sorting algorithm above.
         # We need the length of the longest edge in the quad, the direction it is pointing, and the index of the first vertex of the longest edge (CW order).
         edge_info = __calc_quad_max_edge_len_idx(sorted_verts)
@@ -1783,26 +1788,26 @@ def __calculate_uvs(texture_name, vert_coords, normal, forward_axis):
             # for uv in uvs_sorted:
             #     print("\t", uv)
 
-    elif texture_name == "bottomloop":
+    elif brick_texture is const.BrickTexture.BOTTOMLOOP:
         uvs_sorted = ((0, w),
                       (0, 0),
                       (h, 0),
                       (h, w))
 
-    elif texture_name == "print":
+    elif brick_texture is const.BrickTexture.PRINT:
         uvs_sorted = ((0, 0),
                       (1, 0),
                       (1, 1),
                       (0, 1))
 
-    elif texture_name == "ramp":
+    elif brick_texture is const.BrickTexture.RAMP:
         uvs_sorted = ((0, w),
                       (0, 0),
                       (h, 0),
                       (h, w))
 
     else:
-        raise RuntimeError("Unknown texture name '{}'".format(texture_name))
+        raise RuntimeError("Unknown texture name '{}'".format(brick_texture))
 
     #print("__calculate_uvs | uvs_sorted:")
     # for uv in uvs_sorted:
@@ -2169,7 +2174,7 @@ def __process_definition_objects(properties, objects):
         # PROCESS TOKENS.
 
         obj_name = obj.name
-        obj_name_tokens = __split_object_name_to_tokens(obj_name)
+        obj_name_tokens = __split_object_string_to_tokens(obj_name)
         object_grid_definitions = __get_tokens_from_object_name(obj_name_tokens, properties.grid_def_obj_token_priority)
 
         # Ignore non-mesh objects
@@ -2327,7 +2332,7 @@ def __process_mesh_data(context, properties, bounds_data, mesh_objects, forward_
         colors = None
 
         if properties.blendprop.use_object_colors:
-            tokens = __split_object_name_to_tokens(object_name, True)
+            tokens = __split_object_string_to_tokens(object_name, True)
 
             # Does the object name contain the color definition token signifying that it defines the object's color?
             if properties.blendprop.deftoken_color in tokens:
@@ -2463,22 +2468,22 @@ def __process_mesh_data(context, properties, bounds_data, mesh_objects, forward_
             # ================
             # BLB texture name
             # ================
-            texture_name = None
+            brick_texture = None
 
             if current_mesh.materials and current_mesh.materials[poly.material_index] is not None:
                 matname = current_mesh.materials[poly.material_index].name
-                texnames = __get_tokens_from_object_name(matname, const.VALID_BRICK_TEXTURES)
+                texnames = __get_tokens_from_object_name(matname, const.BrickTexture.as_list())
                 texcount = len(texnames)
 
                 if texcount > 0:
-                    texture_name = texnames[0]
+                    brick_texture = const.BrickTexture[texnames[0]]
 
                     if texcount > 1:
                         logger.info("More than one brick texture name found in material '{}', only the first one is used.".format(matname), 2)
 
-            if texture_name is None:
+            if brick_texture is None:
                 # If no texture is specified, use the SIDE texture as it allows for blank brick textures.
-                texture_name = "side"
+                brick_texture = const.BrickTexture.SIDE
 
             # ===
             # UVs
@@ -2489,7 +2494,7 @@ def __process_mesh_data(context, properties, bounds_data, mesh_objects, forward_
             # Join all material names into one string.
             generated_uv_layer_names = " ".join(current_mesh.materials.keys())
             # Get only the brick texture names.
-            generated_uv_layer_names = __get_tokens_from_object_name(generated_uv_layer_names, const.VALID_BRICK_TEXTURES)
+            generated_uv_layer_names = __get_tokens_from_object_name(generated_uv_layer_names, const.BrickTexture.as_list())
             # List of possible layer names based on the materials of this mesh.
             generated_uv_layer_names = [__string_to_uv_layer_name(texnames) for texnames in generated_uv_layer_names]
 
@@ -2523,15 +2528,15 @@ def __process_mesh_data(context, properties, bounds_data, mesh_objects, forward_
 
             if uvs is None:
                 if properties.blendprop.calculate_uvs:
-                    uvs = __calculate_uvs(texture_name, poly_vertex_obj_coords, poly_normal_normalized, forward_axis)
+                    uvs = __calculate_uvs(brick_texture, poly_vertex_obj_coords, poly_normal_normalized, forward_axis)
 
                     if properties.blendprop.store_uvs:
                         if uvs[1] is not None:
                             # Put the special Blender UVs into the Blender mesh.
-                            __store_uvs_in_mesh(poly.index, current_mesh, uvs[1], __string_to_uv_layer_name(texture_name))
+                            __store_uvs_in_mesh(poly.index, current_mesh, uvs[1], __string_to_uv_layer_name(brick_texture.name))
                         else:
                             # Put the calculated UVs into the Blender mesh.
-                            __store_uvs_in_mesh(poly.index, current_mesh, uvs[0], __string_to_uv_layer_name(texture_name))
+                            __store_uvs_in_mesh(poly.index, current_mesh, uvs[0], __string_to_uv_layer_name(brick_texture.name))
 
                     uvs = uvs[0]
                 else:
@@ -2547,7 +2552,7 @@ def __process_mesh_data(context, properties, bounds_data, mesh_objects, forward_
                 # Object has material slots?
                 if len(obj.material_slots) > 0:
                     material = obj.material_slots[poly.material_index].material
-                    tokens = __split_object_name_to_tokens(material.name)
+                    tokens = __split_object_string_to_tokens(material.name)
 
                     if material is not None:
                         if properties.blendprop.deftoken_color_add in tokens:
@@ -2580,7 +2585,7 @@ def __process_mesh_data(context, properties, bounds_data, mesh_objects, forward_
                         # color_layer.data[index] may contain more than 4 values.
                         loop_color = current_mesh.vertex_colors[0].data[index]
                         layer_name = current_mesh.vertex_colors[0].name.replace(",", ".")
-                        tokens = __split_object_name_to_tokens(layer_name)
+                        tokens = __split_object_string_to_tokens(layer_name)
 
                         if properties.blendprop.deftoken_color_add in tokens:
                             addsub = 1
@@ -2626,7 +2631,7 @@ def __process_mesh_data(context, properties, bounds_data, mesh_objects, forward_
                 raise RuntimeError("Quad color data only defined for {} vertices, 4 required.".format(len(colors)))
 
             # A tuple cannot be used because the values are changed afterwards when the brick is rotated.
-            quads[section.value].append([positions, normals, uvs, colors, texture_name])
+            quads[section.value].append([positions, normals, uvs, colors, brick_texture.name])
 
             if reset_section:
                 section = None

__init__.py
@@ -47,6 +47,7 @@ bl_info = {
 # TODO: Brick quad limit?
 # TODO: Double check all round & cast operations, user defined precision is not always honored.
 # TODO: Check that all docstrings and comments are still up to date.
+# TODO: Write about quad sorting to README.
 
 
 class ExportBLB(bpy.types.Operator, ExportHelper):

const.py
@@ -56,8 +56,24 @@ class BLBQuadSection(IntEnum):
     WEST = 5
     OMNI = 6
 
-# A tuple of valid brick textures in alphabetical order.
-VALID_BRICK_TEXTURES = ("bottomedge", "bottomloop", "print", "ramp", "side", "top")
+
+class BrickTexture(Enum):
+    """Valid brick texture names in alphabetical order."""
+    BOTTOMEDGE = 0
+    BOTTOMLOOP = 1
+    PRINT = 2
+    RAMP = 3
+    SIDE = 4
+    TOP = 5
+
+    def __str__(self):
+        """Returns the name of the enum value in uppercase characters."""
+        return self.name
+
+    @classmethod
+    def as_list(cls):
+        """Returns the names of the members of this enum as a list of uppercase strings."""
+        return [member.name for member in BrickTexture]
 
 # BLB file strings.
 BLB_BRICK_TYPE_SPECIAL = "SPECIAL"

commit 4b244b2c26d37844675581c7e0d067203a7cdb1e

Fixes definition tokens being broken by 21c7ad4ebd0797d76e27fc290e1c1d4622b2c692

blb_processor.py
@@ -960,7 +960,7 @@ def __record_bounds_data(properties, blb_data, bounds_data):
                     "Brick name was to be sourced from the name of the bounds definition object but no brick name was found after the bounds definition (separated with a space), file name used instead.", 1)
             else:
                 # Brick name follows the bounds definition, spaces are not allowed.
-                blb_data.brick_name = name_elements[name_elements.index(properties.blendprop.deftoken_bounds) + 1]
+                blb_data.brick_name = name_elements[name_elements.index(properties.deftokens.bounds) + 1]
                 logger.info("Found brick name from bounds definition: {}".format(blb_data.brick_name), 1)
     elif properties.blendprop.export_count == "MULTIPLE" and properties.blendprop.brick_name_source_multi == "BOUNDS":
         if bounds_data.object_name is None:
@@ -983,7 +983,7 @@ def __record_bounds_data(properties, blb_data, bounds_data):
                         "Brick name was to be sourced from the name of the bounds definition object but no brick name was found after the bounds definition (separated with a space), file name used instead.", 1)
             else:
                 # Brick name follows the bounds definition, spaces are not allowed.
-                blb_data.brick_name = name_elements[name_elements.index(properties.blendprop.deftoken_bounds) + 1]
+                blb_data.brick_name = name_elements[name_elements.index(properties.deftokens.bounds) + 1]
                 logger.info("Found brick name from bounds definition: {}".format(blb_data.brick_name), 1)
 
     return blb_data
@@ -2213,18 +2213,18 @@ def __process_definition_objects(properties, objects):
 
         # Ignore non-mesh objects
         if obj.type != "MESH":
-            if obj_name.startswith(properties.blendprop.deftoken_bounds):
+            if obj_name.startswith(properties.deftokens.bounds):
                 logger.warning("Object '{}' cannot be used to define bounds, must be a mesh.".format(obj_name), 1)
             elif obj_name.startswith(properties.grid_def_obj_token_priority):
                 logger.warning("Object '{}' cannot be used to define brick grid, must be a mesh.".format(obj_name), 1)
-            elif obj_name.startswith(properties.blendprop.deftoken_collision):
+            elif obj_name.startswith(properties.deftokens.collision):
                 logger.warning("Object '{}' cannot be used to define collision, must be a mesh.".format(obj_name), 1)
 
             # Skip the rest of the if.
             continue
 
         # Is the current object a bounds definition object?
-        elif properties.blendprop.deftoken_bounds in obj_name_tokens:
+        elif properties.deftokens.bounds in obj_name_tokens:
             if bounds_data is None:
                 bounds_data = __process_bounds_object(properties.scale, obj)
                 blb_data = __record_bounds_data(properties, blb_data, bounds_data)
@@ -2251,7 +2251,7 @@ def __process_definition_objects(properties, objects):
                 continue
 
         # Is the current object a collision definition object?
-        elif properties.blendprop.deftoken_collision in obj_name_tokens:
+        elif properties.deftokens.collision in obj_name_tokens:
             # Collision definition objects cannot be processed until after the bounds have been defined.
             collision_objects.append(obj)
 
@@ -2371,9 +2371,9 @@ def __process_mesh_data(context, properties, bounds_data, mesh_objects, forward_
             tokens = __split_object_string_to_tokens(object_name, True)
 
             # Does the object name contain the color definition token signifying that it defines the object's color?
-            if properties.blendprop.deftoken_color in tokens:
+            if properties.deftokens.color in tokens:
                 # Parse floats from the expected color values.
-                floats = __get_color_values(tokens[tokens.index(properties.blendprop.deftoken_color) + 1:])
+                floats = __get_color_values(tokens[tokens.index(properties.deftokens.color) + 1:])
                 size = len(floats)
 
                 # Did user define at least 4 numerical values?
@@ -2600,13 +2600,13 @@ def __process_mesh_data(context, properties, bounds_data, mesh_objects, forward_
                     tokens = __split_object_string_to_tokens(material.name)
 
                     if material is not None:
-                        if properties.blendprop.deftoken_color_add in tokens:
+                        if properties.deftokens.color_add in tokens:
                             # Negative alpha.
                             colors = ([(material.diffuse_color.r, material.diffuse_color.g, material.diffuse_color.b, -material.alpha)] * 4)
-                        elif properties.blendprop.deftoken_color_sub in tokens:
+                        elif properties.deftokens.color_sub in tokens:
                             # Negative everything.
                             colors = ([(-material.diffuse_color.r, -material.diffuse_color.g, -material.diffuse_color.b, -material.alpha)] * 4)
-                        elif properties.blendprop.deftoken_color_blank in tokens:
+                        elif properties.deftokens.color_blank in tokens:
                             # If the material name is "blank", use the spray can color by not defining any color for this quad.
                             # This is how quads that can change color (by colorshifting) in DTS meshes (which Blockland uses) are defined.
                             colors = None
@@ -2632,13 +2632,13 @@ def __process_mesh_data(context, properties, bounds_data, mesh_objects, forward_
                         layer_name = current_mesh.vertex_colors[0].name.replace(",", ".")
                         tokens = __split_object_string_to_tokens(layer_name)
 
-                        if properties.blendprop.deftoken_color_add in tokens:
+                        if properties.deftokens.color_add in tokens:
                             addsub = 1
                             # Remove the token and only leave the alpha value.
-                            tokens.remove(properties.blendprop.deftoken_color_add)
-                        elif properties.blendprop.deftoken_color_sub in tokens:
+                            tokens.remove(properties.deftokens.color_add)
+                        elif properties.deftokens.color_sub in tokens:
                             addsub = -1
-                            tokens.remove(properties.blendprop.deftoken_color_sub)
+                            tokens.remove(properties.deftokens.color_sub)
                         else:
                             addsub = 0
 

export_blb.py
@@ -28,21 +28,42 @@ from . import const, logger, blb_processor, blb_writer, common
 
 # The export mediator module.
 
+# ======================
+# DefinitionTokens Class
+# ======================
+class DefinitionTokens(object):
+    """A class storing all definition tokens, except brick grid and quad sort, in upper case strings.
+    Brick grid and quad sort definitions are stored separately in the DerivativeProperties class.
+    """
+
+    def __init__(self, properties):
+        self.bounds = properties.deftoken_bounds.upper()
+        self.collision = properties.deftoken_collision.upper()
+
+        self.color = properties.deftoken_color.upper()
+        self.color_blank = properties.deftoken_color_blank.upper()
+        self.color_add = properties.deftoken_color_add.upper()
+        self.color_sub = properties.deftoken_color_sub.upper()
+
 # ==========================
 # DerivativeProperties Class
 # ==========================
+
 
 class DerivativeProperties(object):
     """A class for storing various properties derived from the user-defined Blender properties to guide the export process.
 
     Stores the following data:
         blendprop (Blender properties object): The original Blender properties object containing more properties that have not been processed further.
-        plate_height (Decimal): The height of one Blockland plate in Blender units.
-        human_error (Decimal): Error allowed for manually created definition objects, because they must lie exactly on the brick grid.
-        scale (Decimal): The scale to export the brick at. Value is in range [0.0,1.0].
+        forward_axis (Axis3D): The axis that will point forwards in-game.
+        deftokens (DefinitionTokens): A class storing all definition tokens, except brick grid and quad sort, in upper case strings.
         grid_def_obj_token_priority (sequence): A sequence containing the user-defined brick grid definitions in reverse priority order.
         grid_definitions_priority (sequence): A sequence containing the brick grid symbols in the same order as grid_def_obj_token_priority.
         quad_sort_definitions (sequence): A sequence containing the user-defined definitions for quad sorting.
+        scale (Decimal): The scale to export the brick at. Value is in range [0.0,1.0].
+        plate_height (Decimal): The height of one Blockland plate in Blender units.
+        human_error (Decimal): Error allowed for manually created definition objects, because they must lie exactly on the brick grid.
+        precision (String): The precision to round floating point numbers to when performing calculations.
     """
 
     def __init__(self, properties):
@@ -61,6 +82,11 @@ class DerivativeProperties(object):
         # Forward Axis
         # ============
         self.forward_axis = const.Axis3D.from_property_name(properties.axis_blb_forward)
+
+        # =================
+        # Definition Tokens
+        # =================
+        self.deftokens = DefinitionTokens(properties)
 
         # ==========
         # Brick Grid
@@ -163,13 +189,13 @@ class DerivativeProperties(object):
             A tuple containing the definitions for manual quad section sorting in the correct order.
         """
         # The definitions must be in the same order as const.QUAD_SECTION_ORDER
-        return (properties.deftoken_quad_sort_top,
-                properties.deftoken_quad_sort_bottom,
-                properties.deftoken_quad_sort_north,
-                properties.deftoken_quad_sort_east,
-                properties.deftoken_quad_sort_south,
-                properties.deftoken_quad_sort_west,
-                properties.deftoken_quad_sort_omni)
+        return (properties.deftoken_quad_sort_top.upper(),
+                properties.deftoken_quad_sort_bottom.upper(),
+                properties.deftoken_quad_sort_north.upper(),
+                properties.deftoken_quad_sort_east.upper(),
+                properties.deftoken_quad_sort_south.upper(),
+                properties.deftoken_quad_sort_west.upper(),
+                properties.deftoken_quad_sort_omni.upper())
 
 
 def __has_object_in_visible_layer(context, objects):